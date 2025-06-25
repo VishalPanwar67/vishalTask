24bc9187c403c2ea88badc88d7e7524963e64542
Vishal Panwar <vishal4panwar@gmail.com>
2025-06-25 22:40:46 +0530

    prepare deveploment

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,8 @@ import contactRoutes from "./routes/contactRoutes.js";
 import subscriptionRoutes from "./routes/subscriptionRoutes.js";
 import cloudinaryConfig from "./Database/Cloudinarydb.js";
 import fileUpload from "express-fileupload";
-import morgan from"morgan";
+import morgan from "morgan";
+import path from "path";
 
 connectDB();
 cloudinaryConfig();
@@ -16,10 +17,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(fileUpload({
-  useTempFiles: true,
-  tempFileDir: '/tmp/'
-}));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 // Routes
 app.use("/api/projects", projectRoutes);
@@ -27,6 +30,16 @@ app.use("/api/clients", clientRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/subscriptions", subscriptionRoutes);
 
-const PORT =  5000;
+const PORT = 5000;
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
